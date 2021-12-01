949854c861f9d3f6bf5b8149249f8b00a49fe6fe
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2021-12-01 21:11:26 +0700

    Created vertical hexagon map

--- generator.py
+++ generator.py
@@ -1,11 +1,24 @@
 import numpy as np
 
+PERCENTAGE_OF_FLOOD = 0.1
+
 
 def generate(seed, mapSize):
     """Создаётся массив с случайными числами, самые большие
-     стремятся к центру массива, от этого генерируется карта, минимум 20% всех клеток
-     по бокам будут морем"""
+     стремятся к центру массива, от этого генерируется карта, минимум 20% всех
+      клеток по бокам будут морем"""
     np.random.seed(seed)
+    mapSize += 2  # так как шестиугольники выходят за границу то без
+    # прибавления получается mapSize - 0.5
     data = np.random.sample((mapSize, mapSize))
-    data[len(data) // 2][len(data[0]) // 2] = 1  # отмечаем центр карты
+    data[mapSize // 2][mapSize // 2] = 1  # отмечаем центр карты
+    for i in range(mapSize):
+        for j in range(round(mapSize * PERCENTAGE_OF_FLOOD)):
+            data[i][j] = 0.0
+            data[i][mapSize - j - 1] = 0.0
+
+    for i in range(round(mapSize * PERCENTAGE_OF_FLOOD)):
+        for j in range(mapSize):
+            data[i][j] = 0.0
+            data[mapSize - i - 1][j] = 0.0
     return data
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 import sys
 from generator import generate
+from math import *
 
 WINDOW_SIZE_X = 500
 WINDOW_SIZE_Y = 500
 FPS = 60
+MAP_SIZE = 25
+SEED = 100
 
 
 def main():
@@ -12,24 +15,55 @@ def main():
     screen = pygame.display.set_mode((WINDOW_SIZE_X, WINDOW_SIZE_Y))
     pygame.display.set_caption('Колонизация')
     running = True
-    land = generate(100, 50)
+    land = generate(SEED, MAP_SIZE)  # это должен вводить пользователь
     while running:
         screen.fill(pygame.Color('white'))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        draw_map(screen, land, WINDOW_SIZE_Y // 50)
+        draw_map(screen, land, WINDOW_SIZE_X / (MAP_SIZE + MAP_SIZE / 2))
         pygame.display.flip()
     pygame.quit()
 
 
 def draw_map(screen, land, sizeOfcell):
-    colors = [pygame.Color('blue'), pygame.Color('green')]
+    sizeOfcell = round(sizeOfcell)
+
+    def calculate_point(center, size, i):
+        angle_deg = 60 * i + 30
+        angle_rad = pi / 180 * angle_deg
+        return center[0] + size * cos(angle_rad),\
+               center[1] + size * sin(angle_rad)
+
+    colors = [pygame.Color('blue'), pygame.Color('green'),
+              pygame.Color('#fcdd76'), pygame.Color('white')]
+
+    shiftY = 0
     for ind, i in enumerate(land):
+        if ind % 2 == 0:
+            shiftX = 0
+        else:
+            shiftX = sqrt(3) / 2 * (sizeOfcell * 2) / 2
         for ind2, j in enumerate(i):
-            pygame.draw.rect(screen, colors[0] if j < 0.3 else colors[1],
-                             (ind2 * sizeOfcell, ind * sizeOfcell, sizeOfcell,
-                              sizeOfcell))
+            if j < 0.15:
+                color = colors[0]
+            elif j < 0.3:
+                color = colors[2]
+            elif j > 0.95:
+                color = colors[3]
+            else:
+                color = colors[1]
+            points = list()
+            for point in range(6):
+                points.append(calculate_point(
+                    [ind2 * sizeOfcell + shiftX,
+                     ind * sizeOfcell + shiftY],
+                    sizeOfcell, point))
+
+            pygame.draw.polygon(screen, color, points)
+            pygame.draw.lines(screen, (0, 0, 0), False, points, 1)
+            shiftX += sizeOfcell / 2
+        shiftY += sizeOfcell / 2
 
 
 if __name__ == '__main__':
