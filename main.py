096e8271a6ad8b3a6009b40f46a44765d79ac142
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2021-12-03 14:52:43 +0700

    Added glowing effect for tiles

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from generator import generate
 from math import *
 from tile import Tile
+import numpy as np
 
 WINDOW_SIZE_X = 750
 WINDOW_SIZE_Y = WINDOW_SIZE_X
@@ -18,14 +19,31 @@ def main():
     running = True
     heights = generate(SEED, MAP_SIZE)  # это должен вводить пользователь
     land = create_map(heights, (WINDOW_SIZE_X / MAP_SIZE) / 1.5)
+    selectedTile = None
     while running:
         screen.fill(pygame.Color('white'))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEMOTION:
+                selectedTile = \
+                    glow_border(event.pos[0], event.pos[1], land, selectedTile)
         draw_map(screen, land)
         pygame.display.flip()
     pygame.quit()
+
+
+def glow_border(x, y, land, selectedTile):
+    list_of_difsX = np.array([abs(x - tile.center[0]) for tile in land])
+    list_of_difsY = np.array([abs(y - tile.center[1]) for tile in land])
+    selectedTileIndex = np.argmin(list_of_difsX + list_of_difsY)
+    if selectedTile is None:
+        land[selectedTileIndex].startGlowing()
+        return land[selectedTileIndex]
+    else:
+        selectedTile.stopGlowing()
+        land[selectedTileIndex].startGlowing()
+        return land[selectedTileIndex]
 
 
 def draw_map(screen, land):
@@ -50,13 +68,14 @@ def create_map(heights, sizeOfcell):
             shiftX = sqrt(3) / 2 * sizeOfcell
         for ind2, j in enumerate(i):
             points = list()
+            centerX = ind2 * sizeOfcell + shiftX
+            centerY = ind * sizeOfcell + shiftY
             for point in range(6):
                 points.append(calculate_point(
-                    [ind2 * sizeOfcell + shiftX,
-                     ind * sizeOfcell + shiftY],
+                    [centerX, centerY],
                     sizeOfcell, point))
 
-            globalMap.append(Tile(j, points))
+            globalMap.append(Tile(j, points, [centerX, centerY]))
             shiftX += sizeOfcell / 2
         shiftY += sizeOfcell / 2
     return globalMap
--- tile.py
+++ tile.py
@@ -4,17 +4,20 @@ import pygame
 colors = {'Sea': pygame.Color('blue'), 'Plains': pygame.Color('green'),
               'Desert': pygame.Color('#fcdd76'),
               'Mountains': pygame.Color('white'),
-              'Forest': pygame.Color('#0a5f38')}
+              'Forest': pygame.Color('#0a5f38'),
+          'Glow': (0, 191, 255), 'Border': (0, 0, 0)}
 
 
 class Tile:
-    def __init__(self, j, cords):
+    def __init__(self, j, cords, center):
         self.height = j
         self.cords = cords
-        print(self.cords)
+        self.Center = center
+        self.borderHardness = 1
         self.__initTile()
 
     def __initTile(self):
+        self.borderColor = colors['Border']
         if self.height < 0.15:  # море
             self.name = 'Sea'
         elif self.height < 0.3:  # пустыня
@@ -27,10 +30,31 @@ class Tile:
             self.name = 'Plains'
         self.color = colors[self.name]
 
+    def startGlowing(self):
+        self.borderColor = colors['Glow']
+        self.borderHardness = 2
+
+    def stopGlowing(self):
+        self.borderColor = colors['Border']
+        self.borderHardness = 1
+
     @property
     def position(self):
         return self.color, self.cords
 
     @property
+    def stats(self):
+        return self.height, self.cords, self.Center
+
+    @property
+    def coordinates(self):
+        return self.cords
+
+    @property
     def borders(self):
-        return (0, 0, 0), False, self.cords, 1
+        return self.borderColor, False, self.cords, self.borderHardness
+
+    @property
+    def center(self):
+        return self.Center
+
